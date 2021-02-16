49591c68bb140a87998c02903f639219ad6cdd15
Bertrand <dan.bertrand.hs@gmail.com>
2021-02-16 15:07:05 +0100

    Created Pages Hoeme/Login/Register + App Components - Set up react-routerdom

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,11 @@
 // import './style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app/index'
+ 
+// provider
 
- 
-const App = () => {
-  return (
-    <h1>Hello</h1>
-  )
-};
- 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />, 
+document.getElementById('root'));
  
